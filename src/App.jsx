4c12f69f694d6a387f02e625ea6a03d49dcae136
fasfashann <saintgloria.jkt@gmail.com>
2024-10-24 00:52:43 +0700

    setup analytics vercel

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import Navbar from "./Navbar";
 import HeroSection from "./HeroSection";
 import SocialProof from "./SocialProof";
 import Services from "./Services";
+import { Analytics } from "@vercel/analytics/react";
 import CallToAction from "./CallToAction";
 function App() {
   return (
     <div className="px-4">
+      <Analytics />
       <Navbar />
       <HeroSection />
       <SocialProof />
