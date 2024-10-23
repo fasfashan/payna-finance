71a64a35611fed4cf68ea40a48b9d65d36af0a11
fasfashann <saintgloria.jkt@gmail.com>
2024-10-23 21:42:03 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p className="text-black text-2xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+        repellat officiis quos dignissimos nostrum aspernatur aperiam amet ex
+        sit corrupti?
+      </p>
+    </>
+  );
+}
+
+export default App;
