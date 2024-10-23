f7622a86c56cc6610d75e06bdac0cf5889802fa8
fasfashann <saintgloria.jkt@gmail.com>
2024-10-24 01:11:07 +0700

    fix bug on mobile responsive

--- src/Services.jsx
+++ src/Services.jsx
@@ -9,66 +9,66 @@ export default function Services() {
               For Your Business
             </h3>
           </div>
-          <p className="text-paragraph  text-wrap">
+          <p className="text-paragraph  break-words">
             We did research what your company needs and here we are providing
             all of them just for you
           </p>
         </div>
-        <div className="grid gap-20 grid-cols-6 mt-20">
-          <div className="flex  items-start lg:col-span-2  md:col-span-3 col-span-6 gap-4">
+        <div className="grid gap-20 grid-cols-1 md:grid-cols-3 lg:grid-cols-6 mt-20">
+          <div className="flex  items-start lg:col-span-2  md:col-span-3  gap-4">
             <img src="/share-insights-icon.svg" alt="" />
             <div className="flex gap-2 flex-col ">
               <h3 className="font-semibold text-xl text-title">
                 Share Insights
               </h3>
-              <p className="text-paragraph  text-wrap">
+              <p className="text-paragraph  break-words">
                 Working together with your team to make decisions
               </p>
             </div>
           </div>
-          <div className="flex  items-start lg:col-span-2  md:col-span-3 col-span-6 gap-4">
+          <div className="flex  items-start lg:col-span-2  md:col-span-3  gap-4">
             <img src="/track-leads-icon.svg" alt="" />
             <div className="flex gap-2 flex-col ">
               <h3 className="font-semibold text-xl text-title">Track Leads</h3>
-              <p className="text-paragraph  text-wrap">
+              <p className="text-paragraph  break-words">
                 See where your money goes and comes in business
               </p>
             </div>
           </div>
-          <div className="flex  items-start lg:col-span-2  md:col-span-3 col-span-6 gap-4">
+          <div className="flex  items-start lg:col-span-2  md:col-span-3  gap-4">
             <img src="/offline-mode.svg" alt="" />
             <div className="flex gap-2 flex-col ">
               <h3 className="font-semibold text-xl text-title">Offline Mode</h3>
-              <p className="text-paragraph  text-wrap">
+              <p className="text-paragraph  break-words">
                 Use the feature while off from internet? sure can
               </p>
             </div>
           </div>
-          <div className="flex  items-start lg:col-span-2  md:col-span-3 col-span-6 gap-4">
+          <div className="flex  items-start lg:col-span-2  md:col-span-3  gap-4">
             <img src="/kanban-mode-icon.svg" alt="" />
             <div className="flex gap-2 flex-col ">
               <h3 className="font-semibold text-xl text-title">Kanban Mode</h3>
-              <p className="text-paragraph  text-wrap">
+              <p className="text-paragraph  break-words">
                 Organize the report that easy to be understand
               </p>
             </div>
           </div>
-          <div className="flex  items-start lg:col-span-2  md:col-span-3 col-span-6 gap-4">
+          <div className="flex  items-start lg:col-span-2  md:col-span-3  gap-4">
             <img src="/reward-system-icon.svg" alt="" />
             <div className="flex gap-2 flex-col ">
               <h3 className="font-semibold text-xl text-title">
                 Reward System
               </h3>
-              <p className="text-paragraph  text-wrap">
+              <p className="text-paragraph  break-words">
                 Motivate your team working harder and receive a gift
               </p>
             </div>
           </div>
-          <div className="flex  items-start lg:col-span-2  md:col-span-3 col-span-6 gap-4">
+          <div className="flex  items-start lg:col-span-2  md:col-span-3  gap-4">
             <img src="/country-icon.svg" alt="" />
             <div className="flex gap-2 flex-col ">
               <h3 className="font-semibold text-xl text-title">189 Country</h3>
-              <p className="text-paragraph  text-wrap">
+              <p className="text-paragraph  break-words">
                 Working together worldwide people from anywhere
               </p>
             </div>
